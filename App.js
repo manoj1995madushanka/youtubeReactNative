e4799006a2077b087c14a9dd6ccbd65a8cf08c01
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-11-15 23:43:38 +0530

    navbar

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Navbar from "./component/Navbar";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+        <View style={styles.container}>
+          <Navbar/>
+        </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
